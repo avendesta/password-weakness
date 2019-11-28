fd11e853547c5f26675884972f3891450b51fd27
Rick Sanchez <noodle-lover>
2019-11-28 20:24:18 +0300

    I made it a function instead of a sample code

--- password_strength.py
+++ password_strength.py
@@ -1,23 +1,21 @@
 import hashlib
 import requests
 
-password = input("Enter your password: ")
-password_hash = hashlib.sha1(password.encode()).hexdigest()
+def weakness(password):
+    password_hash = hashlib.sha1(password.encode()).hexdigest()
 
-first_five = password_hash[:5].upper()
-after_five = password_hash[5:].upper()
+    first_five = password_hash[:5].upper()
+    after_five = password_hash[5:].upper()
+    try:
+        response = requests.get(f"https://api.pwnedpasswords.com/range/{first_five}",timeout=0.1)
+    except requests.exceptions.RequestException as e:
+        return 0
+    else:
+        content = response.text
+        weakness = 0
 
-response = requests.get(f"https://api.pwnedpasswords.com/range/{first_five}")
-content = response.text
-
-weakness = 0
-
-for line in content.splitlines():
-    if after_five in line:
-        weakness = line.split(':')[1]
-        break
-
-if weakness is not 0:
-    print(f"The password you used is common. It has been used {weakness} times")
-else:
-    print("Your password is safe!")
+        for line in content.splitlines():
+            if after_five in line:
+                _ , weakness = line.split(':')
+                break
+        return int(weakness)
